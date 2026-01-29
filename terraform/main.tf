3218595b58a95088815d89d63b8e71a5f1624632
root <root@ip-172-31-75-146.ap-south-1.compute.internal>
2026-01-29 09:24:26 +0000

    changes

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+
+
+terraform {
+
+  required_version = ">= 1.0.0"
+
+}
+
+
+
+output "message" {
+
+  value = "Project 6: Version Control Infrastructure"
+
+}
+
+
+
+
+output "version" {
+
+  value = "v2 - feature branch update"
+
+}
+
+
